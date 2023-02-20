840402dd09f5cdd7e90e1079983bdcd2549071ff
Michel Bernardo <michel_13s@outlook.com>
2023-02-19 22:17:50 -0300

    Adds time and date in alert

--- main.go
+++ main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/martinlindhe/notify"
 )
 
+func Time() string {
+	now := time.Now()
+	Date := now.AddDate(0, 0, -7).Format("2006-01-02")
+	Time := time.Now().Format("02/Jan/2006")
+	var dateTime []string
+	dateTime = append(dateTime, Date, Time)
+	dateTimeFormat := strings.Join(dateTime, " | ")
+	return dateTimeFormat
+}
+
 func main() {
-	fmt.Print("Mensagem de notificação: ")
 	var message string
+	var setTimeDurationToAlert int
+
+	fmt.Print("Notification message: ")
 	fmt.Scan(&message)
 
-	fmt.Print("Daqui a quantos segundos: ")
-	var setTimeDuration int
-	fmt.Scan(&setTimeDuration)
+	fmt.Print("In how many minutes:: ")
+	fmt.Scan(&setTimeDurationToAlert)
 
-	time.Sleep(time.Duration(setTimeDuration) * time.Second)
-	notify.Alert("_", "ALERT!", "Lembrete: "+message, "path/to/icon.png")
-
+	time.Sleep(time.Duration(setTimeDurationToAlert) * time.Second)
+	notify.Alert(Time(), "ALERT!", "Reminder: "+message, "path/to/icon.png")
 }
